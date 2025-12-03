4b16a6aaac6d6a12ef5e1422ae7f92c09b4c5377
pagalladka78901-maker <pagalladka78901@gmail.com>
2025-12-03 18:09:27 +0530

    Update 0pair.js

--- Priyansh/commands/0pair.js
+++ Priyansh/commands/0pair.js
@@ -79,5 +79,5 @@ module.exports.run = async function({ api, event, args, models, Users, Threads, 
 var one = senderID, two = id;
     return makeImage({ one, two }).then(path => api.sendMessage({ body: `♡   ∩_∩  
  （„• ֊ •„)♡  
-╭─∪∪────♡──────⟡      │ ${namee} ├───────♡──────⟡                           │ ${name}\n🍓 ╰───────♡──────⟡  ${tle}`, mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+╭─∪∪────♡──────⟡      │ ${namee} ├───────♡──────⟡                           │ ${name}\n╰───────♡──────⟡  ${tle}`, mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
   }
